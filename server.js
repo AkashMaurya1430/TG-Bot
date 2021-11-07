f9598fbf9c5b76538b73d3a1a2c76d6649eb7221
AkashMaurya1430 <akashmaurya1430@gmail.com>
2021-11-07 20:08:44 +0530

    Base Route

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var bodyParser = require("body-parser");
 var path = require("path");
 const logger = require("morgan");
 var app = express();
-const cors = require("cors");
 
 // Set up middleware
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,25 +14,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, "../public")));
 
-const allowedOrigins = ["https://grifftips.com/index.php/about/", "http://localhost:8100"];
-
-// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Origin not allowed by CORS"));
-    }
-  },
-};
-
-// Enable preflight requests for all routes
-app.options("*", cors(corsOptions));
-
 // --------------ROUTES-------------------
+app.get("/", function (req, res) {
+  res.send("Server Running");
+});
+
 app.get("/page1", function (req, res) {
-  res.render("page1.ejs");
+  res.render("index.ejs");
 });
 
 app.get("/page2", function (req, res) {
